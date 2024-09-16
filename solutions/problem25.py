74c28adbf0d6591cfd3f6dd39845d24dcaa9c5c3
ravi0the0sun <ravi3pun100@gmail.com>
2024-09-17 09:18:56 +1000

    Solved Problem 25

---
+++ solutions/problem25.py
@@ -0,0 +1,20 @@
+DIGITS = 1000
+
+"""
+    RecursionError: maximum recursion depth exceeded while getting the str of an object
+"""
+
+def fib(n, p, index):
+    if len(str(n)) > DIGITS - 1:
+        return index
+    return fib(n + p, n, index + 1)
+
+def fib():
+    n = 0
+    p = 1
+    index = 0
+    while len(str(n)) < DIGITS:
+        n, p, index = n + p, n, index + 1
+    return index
+
+print(fib())
